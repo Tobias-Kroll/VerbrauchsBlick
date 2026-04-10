7abf7a32116241704985382cc71f1b2224e12c44
Tobias Kroll <tobias.kroll2@stud.htw-dresden.de>
2026-04-10 20:04:31 +0200

    added Contractview

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { IonApp, IonContent } from '@ionic/vue'
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './views/Contract.vue'
 </script>
 
 <template>
